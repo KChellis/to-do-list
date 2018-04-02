fbd790a5b3578a37bb1b60c9afb97fab8c2b09ee
Kristen Chellis and Matt Miller <kristen@>
2018-04-02 14:28:38 -0700

    Add function to list notes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-function Task(task, date){
+function Task(task, date, note){
   this.task=task;
   this.date=date;
+  this.note=note;
+}
+
+Task.prototype.noteArray = function() {
+  return this.note.split(", ");
 }
 
 $(function(){
@@ -9,15 +14,18 @@ $(function(){
 
     var task= $("#new-task").val();
     var date= $("#due-date").val();
-    var newTask = new Task(task, date);
-    $("#tasks-list").append("<li class='task'>" + newTask.task + "</li>");
+    var note= $("#task-note").val();
+    var newTask = new Task(task, date, note);
+    var notes = newTask.noteArray();
+    $(".tasks-list").append("<h5>" + newTask.task + "</h5><div class='hidden'> <p>Complete this task by " + newTask.date + "</p> <h5>Notes:</h5><ul id='notes-list'></ul></div>");
+    for (var i = 0; i < notes.length; i++) {
+      $("#notes-list").append("<li>" + notes[i] + "</li>")
+    }
 
     $("#new-task").val("");
 
     $(".task").last().click(function() {
-      $(this).remove();
+
     });
-
-
   });
 });
