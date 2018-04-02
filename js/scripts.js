147d8984717e4ea1555712198dbfd954b90ca627
Kristen Chellis and Matt Miller <matt@>
2018-04-02 13:59:01 -0700

    remove nonfunctioning code

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,13 @@ $(function(){
     var task= $("#new-task").val();
     var date= $("#due-date").val();
     var newTask = new Task(task, date);
-    $("#tasks-list").append("<li class='task'>" + newTask.task + "<button type='button' class='btn btn-secondary' data-toggle='modal' data-target= '#infoModal'>Info</button></li>");
+    $("#tasks-list").append("<li class='task'>" + newTask.task + "</li>");
 
     $("#new-task").val("");
-    // $('#infoModal').on('shown.bs.modal', function () {
-    // });
-    // $(".modalButton").click(function(){
-    //   $("#infoModal").modal("show");
-    // });
 
-    // $(".task").last().click(function() {
-    //   $(this).remove();
-    // });
+    $(".task").last().click(function() {
+      $(this).remove();
+    });
 
 
   });
